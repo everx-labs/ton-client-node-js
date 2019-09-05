0ea377b4f0c0efb2b91b5a8f675466c17c0e5bd2
Michael Vlasov <melsomino@gmail.com>
2019-09-05 19:21:38 +0500

    Initial

--- install.js
+++ install.js
@@ -1,1 +1,96 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const https = require('https');
+const http = require('http');
+const zlib = require('zlib');
+
+const p = os.platform();
+const v = process.env.npm_package_version.split('.').join('_');
+const root = process.cwd();
+const binariesHost = 'sdkbinaries.tonlabs.io';
+
+
+function downloadAndGunzip(dest, url) {
+    return new Promise((resolve, reject) => {
+
+        const request = http.get(url, response => {
+            if (response.statusCode !== 200) {
+                reject({
+                    message: `Download failed with ${response.statusCode}: ${response.statusMessage}`,
+                });
+                return;
+            }
+            fs.mkdirSync(path.dirname(path.resolve(dest)), { recursive: true });
+            let file = fs.createWriteStream(dest, { flags: "w" });
+            let opened = false;
+            const failed = (err) => {
+                if (file) {
+                    file.close();
+                    file = null;
+
+                    fs.unlink(dest, () => {
+                    });
+                    reject(err);
+                }
+            };
+
+            const unzip = zlib.createGunzip();
+            unzip.pipe(file);
+
+
+            response.pipe(unzip);
+
+
+            request.on("error", err => {
+                failed(err);
+            });
+
+            file.on("finish", () => {
+                if (opened && file) {
+                    resolve();
+                }
+            });
+
+            file.on("open", () => {
+                opened = true;
+            });
+
+            file.on("error", err => {
+                if (err.code === "EEXIST") {
+                    file.close();
+                    reject("File already exists");
+                } else {
+                    failed(err);
+                }
+            });
+        });
+    });
+
+}
+
+
+async function dl(dst, src) {
+    const dst_path = `${root}/${dst}`;
+    const src_url = `http://${binariesHost}/${src}.gz`;
+    process.stdout.write(`Downloading ${dst} from ${binariesHost} ...`);
+    await downloadAndGunzip(dst_path, src_url);
+    process.stdout.write('\n');
+}
+
+async function main() {
+    await dl(`tonclient.node`, `tonclient_${v}_nodejs_addon_${p}`);
+    if (p === 'darwin') {
+        await dl('libtonclientnodejs.dylib', `tonclient_${v}_nodejs_dylib_${p}`);
+    }
+}
+
+(async () => {
+    try {
+        await main();
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+})();
 
