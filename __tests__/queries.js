3435be8bd4f90864d9ff683dcd950ec0dbe1d3e3
Michael Vlasov <melsomino@gmail.com>
2019-09-06 17:42:57 +0500

    Initial

--- __tests__/queries.js
+++ __tests__/queries.js
@@ -6,7 +6,7 @@ beforeAll(init);
 test('subscribe', async () => {
     const ton = TONClient.shared;
     const subscription = ton.queries.blocks.subscribe({
-    }, '_key', (e, doc) => {
+    }, 'id', (e, doc) => {
         console.log('>>>', doc);
     });
     return new Promise((resolve) => {
@@ -21,9 +21,9 @@ test('subscribe', async () => {
 test('Transaction List', async () => {
     const ton = TONClient.shared;
     const transaction = await ton.queries.transactions.query({
-            _key: { eq: 'e19948d53c4fc8d405fbb8bde4af83039f37ce6bc9d0fc07bbd47a1cf59a8465'},
+            id: { eq: 'e19948d53c4fc8d405fbb8bde4af83039f37ce6bc9d0fc07bbd47a1cf59a8465'},
             status: { in: ["Preliminary", "Proposed", "Finalized"] }
-    }, '_key now status', [], 1);
+    }, 'id now status', [], 1);
     console.log('>>>', transaction);
     // expect(transaction[0].id).toEqual('e19948d53c4fc8d405fbb8bde4af83039f37ce6bc9d0fc07bbd47a1cf59a8465');
 });
@@ -31,14 +31,14 @@ test('Transaction List', async () => {
 test('All Accounts', async () => {
     const ton = TONClient.shared;
     const docs = await ton.queries.accounts.query({
-    }, '_key storage { balance { Grams } }');
+    }, 'id storage { balance { Grams } }');
     console.log('>>>', docs);
 });
 
 test('Message', async () => {
     const ton = TONClient.shared;
     const messages = await ton.queries.messages.query({
-            _key: { eq: '3a8e38b419a452fe7a0073e71c083f926055d0f249485ab9f8ca6e9825c20b8c' }
+            id: { eq: '3a8e38b419a452fe7a0073e71c083f926055d0f249485ab9f8ca6e9825c20b8c' }
     }, 'body header { ...on MessageHeaderExtOutMsgInfoVariant { ExtOutMsgInfo { created_at } } }');
     expect(messages[0].header.ExtOutMsgInfo.created_at).toEqual(1562342740);
 });
@@ -55,7 +55,7 @@ test('Wait For', async () => {
     const ton = TONClient.shared;
     const data = await ton.queries.transactions.waitFor({
         now: { gt: 1563449 },
-    }, '_key status');
+    }, 'id status');
     console.log('>>>', data);
     expect(data.status).toEqual('Finalized');
 });
