29fb62f76952de4beeafdba3c576dd92f158a5e1
Michael Vlasov <melsomino@gmail.com>
2019-09-09 23:19:47 +0500

    Copyright

--- __tests__/init.js
+++ __tests__/init.js
@@ -22,7 +22,6 @@ export default async () => {
     TONClient.shared.config.setData({
         defaultWorkchain: 0,
         servers: ["http://0.0.0.0"],
-        queriesServer: "http://0.0.0.0:4000/graphql",
         log_verbose: true,
     });
     await TONClient.shared.setup();
