ebb64d3c3cd31cfa4776ca33f67fde837093420c
Eugenia Makarova <makarova.evgenia@gmail.com>
2020-05-19 15:53:44 +0300

    refactoring TON_SDK_BIN_VERSION

--- __tests__/test-binaries.js
+++ __tests__/test-binaries.js
@@ -1,17 +1,5 @@
 /*
  * Copyright 2018-2020 TON DEV SOLUTIONS LTD.
- *
- * Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
- * this file except in compliance with the License.  You may obtain a copy of the
- * License at:
- *
- * http://www.ton.dev/licenses
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific TON DEV software governing permissions and
- * limitations under the License.
  */
 
 import {TONClient} from '../index.js'
@@ -23,9 +11,9 @@ test("Binaries", async () => {
     const client = await TONClient.create({ servers: [] });
     const version = await client.config.getVersion();
     console.log(`version: ${version}`)
-    if (process.env.BINARIES_VERSION) {
+    if (process.env.TON_SDK_BIN_VERSION) {
         expect(version)
-            .toEqual(process.env.BINARIES_VERSION);
+            .toEqual(process.env.TON_SDK_BIN_VERSION);
     } else {
         expect(version.split('.')[0])
             .toEqual(bv);
